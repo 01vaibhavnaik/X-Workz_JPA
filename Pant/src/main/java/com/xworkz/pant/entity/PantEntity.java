b906fde81c5b6c5039b40005a3a64b09dcc1515c
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-03 18:00:25 +0530

    Entiy Of JPA

---
+++ Pant/src/main/java/com/xworkz/pant/entity/PantEntity.java
@@ -0,0 +1,16 @@
+package com.xworkz.pant.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "pant")
+public class PantEntity {
+
+}
