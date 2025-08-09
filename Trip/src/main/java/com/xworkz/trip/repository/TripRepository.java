9e9e721bde0221e098a07fcb6a680a0f0640da4c
01vaibhavnaik <vaibhavnaik32275@gmail.com>
2025-08-09 14:41:22 +0530

    Trip Data Adding In DB

--- Trip/src/main/java/com/xworkz/trip/repository/TripRepository.java
+++ Trip/src/main/java/com/xworkz/trip/repository/TripRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface TripRepository {
     void save(List<TripEntity> tripEntities);
     List<TripEntity> getAll();
+    TripEntity getByName(String name);
 }
--- Trip/src/main/java/com/xworkz/trip/service/TripService.java
+++ Trip/src/main/java/com/xworkz/trip/service/TripService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface TripService {
     void save(List<TripEntity> tripEntities);
    List <TripEntity> getAll();
+   TripEntity getByName(String name);
 
 
 }
